b9ee8044d1bb8e780d122484a6c5071930bdf640
John-Green135 <jsg1982430@yahoo.com>
2021-05-30 00:30:07 -0500

    M

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded( {extended: true }) );
 
-app.get('/', (req, res)=>{
+app.get('/', cors(), (req, res)=>{
     console.log("hit")
-    res.send("message")
+    res.json({message:"message"})
 })
 
   app.listen(3000, () => console.log('Example app is listening on port 3000.'));
